d49cd1e45bc7ee50c1c3eeb481af099bf5c51295
Nilesh <nileshchaudhary1905@gmail.com>
2020-12-18 23:23:39 +0530

    login/signup

--- CreativeScrapyard/Home/urls.py
+++ CreativeScrapyard/Home/urls.py
@@ -7,6 +7,8 @@ app_name="Home"
 
 urlpatterns = [
     path('',home,name="home"),
+    path('login/',login,name="login"),
+    path('signup/',signup,name="signup"),
     path('shop/',include('Items.urls')),
 
 ]
--- CreativeScrapyard/Home/views.py
+++ CreativeScrapyard/Home/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render,redirect
 def home(request):
     template="Home/index.html"
     return render(request,template)
+
+def login(request):
+    template="Home/login.html"
+    return render(request,template)
+
+def signup(request):
+    template="Home/registration.html"
+    return render(request,template)
