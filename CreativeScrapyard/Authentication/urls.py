d4c1506dcb95fa52a26eacce41888445896cbe1f
Nikul3201 <nikulprajapati1901@gmail.com>
2020-12-28 20:44:31 +0530

    dashboard files

--- CreativeScrapyard/Authentication/urls.py
+++ CreativeScrapyard/Authentication/urls.py
@@ -6,6 +6,12 @@ app_name = "Authentication"
 urlpatterns = [
     path('profile/', profile, name="profile"),
     path('dashboard/', dashboard, name="dashboard"),
-    path('dashboard/add-product/', add_product, name="add_product"),
+    path('dashboard/product/creative/', creative_items, name="creative_items"),
+    path('dashboard/product/creative/add', add_creative_product, name="add_creative_product"),
     path('dashboard/add-document/', add_document, name="add_document"),
+    path('dashboard/profile/', dashboard_profile, name="dashboard_profile"),
+    path('dashboard/orders/creative/', order_creative, name="order_creative"),
+    path('dashboard/orders/history/', order_history, name="order_history"),
+    path('dashboard/orders/details/', order_details, name="order_details"),
+    path('dashboard/settings/', settings, name="settings"),
 ]
--- CreativeScrapyard/Authentication/views.py
+++ CreativeScrapyard/Authentication/views.py
@@ -5,15 +5,38 @@ def profile(request):
     template = "account/profile.html"
     return render(request, template)
 
+def creative_items(request):
+    template = "account/dashboard/creative-items.html"
+    return render(request, template)
 
-def add_product(request):
+def add_creative_product(request):
     template = "account/dashboard/add-product.html"
     return render(request, template)
 
 def dashboard(request):
-    template = "account/dashboard.html"
+    template = "account/dashboard/dashboard.html"
     return render(request, template)
 
 def add_document(request):
     template = "account/dashboard/document.html"
+    return render(request, template)
+
+def dashboard_profile(request):
+    template = "account/dashboard/dashboard-profile.html"
+    return render(request, template)
+
+def order_creative(request):
+    template = "account/dashboard/creative-orders.html"
+    return render(request, template)
+
+def order_history(request):
+    template = "account/dashboard/order-history.html"
+    return render(request, template)
+
+def order_details(request):
+    template = "account/dashboard/order-details.html"
+    return render(request, template)
+
+def settings(request):
+    template = "account/dashboard/settings.html"
     return render(request, template)
