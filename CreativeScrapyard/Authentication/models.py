d7d7759d320a2dcb81c1b8dfd5592baf19d9eb01
Nilesh <nileshchaudhary1905@gmail.com>
2021-02-07 19:04:56 +0530

    misc

--- CreativeScrapyard/Authentication/models.py
+++ CreativeScrapyard/Authentication/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 GENDER_TYPE = (
